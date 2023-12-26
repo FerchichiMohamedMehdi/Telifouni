3d0b5c8b68db8eced6ba5a107be77ac2ddff71eb
FerchichiMohamedMehdi <mohamedmehdi.ferchichi@etudiant-isi.utm.tn>
2023-12-26 23:54:07 +0100

    init

---
+++ src/main/java/com/example/telifouni/TelifouniApplication.java
@@ -0,0 +1,13 @@
+package com.example.telifouni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelifouniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelifouniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/telifouni/TelifouniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telifouni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelifouniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
